db5fbade179c2f6c0185474852e8649577b0df85
Abdulaziz <31155566+abdulazoz@users.noreply.github.com>
2019-05-07 17:31:20 +0300

    Procfile for Heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
 
   <!-- Bootstrap CSS File -->
   <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css" integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If" crossorigin="anonymous">
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
 
 
   <!-- Libraries CSS Files -->
