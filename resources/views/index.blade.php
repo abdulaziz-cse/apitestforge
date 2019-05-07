183bb1d359e3d5c2290daf6409f18d4a3678d6d7
Abdulaziz <31155566+abdulazoz@users.noreply.github.com>
2019-05-07 17:27:28 +0300

    Procfile for Heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,6 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <!-- Main Stylesheet File -->
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/style2.css') }}" rel="stylesheet">
 
 
 </head>
