f94368b04c62a19278f6c0c0677532d9042e9dfa
Abdulaziz <31155566+abdulazoz@users.noreply.github.com>
2019-05-12 02:16:38 +0300

    dkkkd

--- database/migrations/2019_05_11_220109_create_posts_table.php
+++ database/migrations/2019_05_11_220109_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('patrs', 100)->nullable();
+            $table->string('time', 100)->nullable();
+            $table->string('name', 100)->nullable();
+            $table->string('number',10)->nullable();
+            $table->boolean('approved')->default(0);
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
