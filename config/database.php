4887e156066c91a49224fac6a0018a6d3ef8e131
Abdulaziz <31155566+abdulazoz@users.noreply.github.com>
2019-05-12 02:42:39 +0300

    dkkkd

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_0f533f307d91c20'),
+            'username' => env('DB_USERNAME', 'b29f2c7a100096'),
+            'password' => env('DB_PASSWORD', 'acae0a82'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
